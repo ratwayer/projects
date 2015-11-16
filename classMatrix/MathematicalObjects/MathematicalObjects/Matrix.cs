92a0219d1cb27e3679e0ec422bc7fba880f24181
ratwayer <gmakariiii@mail.ru>
2015-11-16 12:09:57 +0300

    task - matrix

---
+++ classMatrix/MathematicalObjects/MathematicalObjects/Matrix.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalObjects
+{
+    class Matrix
+    {
+        private int[,] matrix;
+        private int numberOfRows, numberOfColumns;
+
+        public Matrix(int numberOfRows, int numberOfColumns)
+        {
+            if (numberOfRows < 1 || numberOfRows>10000)
+                numberOfRows = 2;
+            if (numberOfColumns < 1 || numberOfColumns > 10000)
+                numberOfColumns = 2;
+
+            this.numberOfRows = numberOfRows;
+            this.numberOfColumns = numberOfColumns;
+            matrix = new int[numberOfRows, numberOfColumns];
+        }
+
+        public int this[int indexI, int indexJ]
+        {
+            set { matrix[indexI, indexJ] = value; }
+            get { return matrix[indexI, indexJ]; }
+        }
+
+        public int NumberOfRows
+        {
+            get { return numberOfRows; }
+        }
+
+        public int NumberOfColumns
+        {
+            get { return numberOfColumns; }
+        }
+
+        public void FillMatrix()
+        {
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    matrix[i, j] = randomNumber.Next(10);
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        public virtual void ShowMatrix()
+        {
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    Console.Write("{0,4}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            Matrix c = new Matrix(a.numberOfRows, a.NumberOfColumns);
+
+            for (int i = 0; i < a.numberOfRows; i++)
+            {
+                for (int j = 0; j < a.NumberOfColumns; j++)
+                {
+                    c[i, j] = a[i, j] + b[i, j];
+                }
+            }
+
+            return c;
+        }
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            Matrix c = new Matrix(a.numberOfRows, a.NumberOfColumns);
+
+            for (int i = 0; i < a.numberOfRows; i++)
+            {
+                for (int j = 0; j < a.NumberOfColumns; j++)
+                {
+                    c[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return c;
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            
+            Matrix c = new Matrix(a.numberOfRows, b.NumberOfColumns);
+            
+            for (int i = 0; i < a.numberOfRows; i++)
+            {
+                for (int j = 0; j < b.numberOfColumns; j++)
+                {
+                    for (int k = 0; k < b.numberOfRows; k++)
+                    {
+                        c[i, j] += a[i, k] * b[k, j];
+                    }
+                }    
+            }
+            return c;
+        }
+
+        public override string ToString()
+        {
+            string s = null ;
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                {
+                    s += Convert.ToString(matrix[i, j]) + " ";
+                    
+                }
+                s += "\n\n";
+            }
+            return s;
+        }
+    }
+}
---
+++ classMatrix/MathematicalObjects/MathematicalObjects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix a = new Matrix(6,7);
+            Matrix b = new Matrix(7,6);
+            Matrix c;
+
+            Console.WriteLine("Матрица A:");
+            a.FillMatrix();
+            Console.WriteLine(a);
+
+            Console.WriteLine("Матрица B:");
+            b.FillMatrix();
+            Console.WriteLine(b);
+
+            if ((a.NumberOfRows == b.NumberOfRows) && (a.NumberOfColumns == b.NumberOfColumns))
+            {
+                Console.WriteLine("Сумма");
+                c = a + b;
+                c.ShowMatrix();
+                Console.WriteLine("Разность");
+                c = a - b;
+                c.ShowMatrix();
+            }
+            else
+            {
+                Console.WriteLine("Матрицы неодинаковы по размеру.Сложение и вычитание невозможно.\n");
+            }
+
+            if (a.NumberOfColumns == b.NumberOfRows)
+            {
+                Console.WriteLine("Произведение");
+                c = a * b;
+                c.ShowMatrix();
+            }
+            else
+            {
+                Console.WriteLine("Количество столбцов матрицы A не равно количеству строк матрицы B.Умножение невозможно.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
